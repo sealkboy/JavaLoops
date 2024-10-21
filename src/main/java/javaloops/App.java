b7d146f7380814dba2bbbab13fba329076a37f79
Seal Kevin Boy Abad <sealkevinboyabad@gmail.com>
2024-10-21 22:47:31 +0200

    Commit incial

---
+++ src/main/java/javaloops/App.java
@@ -0,0 +1,13 @@
+package javaloops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
