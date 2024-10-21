c26e79578ee5a5047b8ffd87591702238d97765a
Seal Kevin Boy Abad <sealkevinboyabad@gmail.com>
2024-10-21 23:43:07 +0200

    Modificación del código de JavaLoops.java

--- src/main/java/javaloops/JavaLoops.java
+++ src/main/java/javaloops/JavaLoops.java
@@ -1,10 +1,10 @@
 package javaloops;
 
 public class JavaLoops {
-    public String[] tablaMultiplicar (int n) {
-        String[] tabla = new String[10]; 
-        for (int i = 0; i < 10; i++) {
-            tabla[i] = n + " x " + (i+1) + " = " + (n * (i+1));
+    public String tablaMultiplicar (int n) {
+        String tabla = "";
+        for (int i = 0; i <= 10; i++) {
+            tabla += n + " x " + i + " = " + (n * i) + "\n";
         }
         return tabla;
     } 
