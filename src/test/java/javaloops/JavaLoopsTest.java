93b1ae7696828155b8c23509c011b842c3d33d42
Seal Kevin Boy Abad <sealkevinboyabad@gmail.com>
2024-10-21 23:49:24 +0200

    añadido clase tests2

--- src/test/java/javaloops/JavaLoopsTest.java
+++ src/test/java/javaloops/JavaLoopsTest.java
@@ -4,17 +4,25 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class JavaLoopsTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class JavaLoopsTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testTablaMultiplicar() {
+        JavaLoops javaLoops = new JavaLoops();
+        String expected = "5 x 0 = 0\n" +
+                          "5 x 1 = 5\n" +
+                          "5 x 2 = 10\n" +
+                          "5 x 3 = 15\n" +
+                          "5 x 4 = 20\n" +
+                          "5 x 5 = 25\n" +
+                          "5 x 6 = 30\n" +
+                          "5 x 7 = 35\n" +
+                          "5 x 8 = 40\n" +
+                          "5 x 9 = 45\n" +
+                          "5 x 10 = 50\n";
+        String result = javaLoops.tablaMultiplicar(5);
+        
+        assertEquals(expected, result);
+        
     }
 }
